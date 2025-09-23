538d0d29c669e0bf009cb8a123ec9e990b9b88d6
deepika <pakkelagnanadeepika@gmail.com>
2025-09-23 23:41:47 +0530

    uap

--- sportsfrontend/vite.config.js
+++ sportsfrontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "reactsportsdb", // updated from reactf1racedb
+  base: "/reactsportsdb/", // updated from reactf1racedb
 })
